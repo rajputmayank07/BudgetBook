79da4a1532f26af8a34e5dcf1734b215c5c1a510
Mayank <mayank2002singh@gmail.com>
2024-12-27 12:22:09 +0530

    changed image save path and used mongostore for session

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ import fs from 'fs';
 import express from "express";
 import cors from "cors";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import groupRoutes from "./routes/groupRoutes.js";
 import connectDB from "./backend/src/config/db.js";
 import bodyParser from "body-parser";
 import { getSpendingInsights } from "./controllers/insightsController.js";
 import path from "path";
-
+import morgan from "morgan";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -29,23 +30,45 @@ import {
 const app = express();
 const port =process.env.PORT || 5000;
 
+//for logging 
+app.use(morgan("combined"));
+
 app.use(cors());
 app.use(express.json());
-
-app.use(
-  session({
-    secret: "mysecretkey",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-
+// Serve the public/images directory
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(express.urlencoded({ extended: false }));
+
 console.log("Attempting to start the server...");
 connectDB();
+
+// app.use(
+//   session({
+//     secret: "mysecretkey",
+//     resave: false,
+//     saveUninitialized: true,
+//   })
+// ); this was messagetore, below is mongostore
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      collectionName: "sessions",
+    }),
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, // 1 day
+    },
+  })
+);
+
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
@@ -56,9 +79,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 app.post("/register", registerController);
 app.post("/login", loginController);
 app.post("/delete", deleteUserController);
@@ -74,25 +94,24 @@ app.post("/fetchBudgets", fetchBudgets);
 // group routes
 app.use("/api/groups", groupRoutes);
 app.post("/save-image", async (req, res) => {
-  const { image } = await req.body;
-
-  // Decode the data URL and save the image to the public/images/ directory
+  const { image } = req.body;
   const base64Data = image.replace(/^data:image\/png;base64,/, "");
-  fs.writeFile(
-    "C:/Users/vishe/Desktop/Mayank_Workspace/ReactProject/src/images/captured-chart.png",
-    base64Data,
-    "base64",
-    (err) => {
-      if (err) {
-        console.error("Error saving the image:", err);
-        res.status(500).json({ error: "Failed to save the image" });
-      } else {
-        console.log("Image saved successfully");
-        res.status(200).json({ message: "Image saved successfully" });
-      }
+  const imageSavePath = path.join(__dirname,"public", "images", "captured-chart.png");
+
+  fs.writeFile(imageSavePath, base64Data, "base64", (err) => {
+    if (err) {
+      console.error("Error saving the image:", err);
+      res.status(500).json({ error: "Failed to save the image" });
+    } else {
+      console.log("Image saved successfully");
+      res.status(200).json({ message: "Image saved successfully" });
     }
-  );
+  });
 });
 
 
 app.post("/api/insights", getSpendingInsights);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
